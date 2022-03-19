e577d5c7b51ea9f073c9908608192b1af94ad20b
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-03-19 08:59:20 +0100

    section 4 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 import FeedbackData from './data/FeedbackData'
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
-    const handleDelete = (id) => {
-        if (window.confirm('Are you sure you want to delete this comment?')) {
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const handleDelete = (id) => {
+    if (window.confirm('Are you sure you want to delete this comment?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
     }
-    
-    return (
-        <>
-            <Header />
-            <FeedbackStats feedback={feedback} />
-            <FeedbackList feedback={feedback} handleDelete={handleDelete} />
-        </>
-    )
+  }
+
+  return (
+    <>
+      <Header />
+      <FeedbackForm handleAdd={addFeedback} />
+      <FeedbackStats feedback={feedback} />
+      <FeedbackList feedback={feedback} handleDelete={handleDelete} />
+    </>
+  )
 }
 export default App
