3e1c0e6d1e171f25e52370f3f147f52c0da0f995
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-03-24 12:24:39 +0100

    add logout button

--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,16 +1,34 @@
 import { getAuth } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
-import { useEffect } from 'react'
+
 
 function Profile() {
-  const [user, setUser] = useState(null)
   const auth = getAuth()
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
 
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
+  const { name, email } = formData
 
-  return user ? <h1>{user.displayName}</h1> : 'not logged in'
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  return (
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  )
 }
 
 export default Profile
